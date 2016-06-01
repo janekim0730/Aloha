9674303ff1bdb92f17ddd0a7cecedd985915e9cd
janekim0730 <janekim0730@gmail.com>
2016-06-01 16:28:50 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+
+// bxslider
+$(function() {
+		$('.newsletter').on('submit', 'form', function(event) {
+			event.preventDefault();
+			var $emailInput = $('#your-email');
+
+			if ( $emailInput.val().length !== 0 ) {
+				alert('Thank you for subscribing!');
+				$emailInput.val('');
+			} else {
+				alert('Please submit a valid email address.');
+			}
+		});
+	});
+
+//smooth scrolling
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
